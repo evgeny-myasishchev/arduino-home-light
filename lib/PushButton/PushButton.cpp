842163406054c71365691e7927c253919f4aa51c
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-04-10 00:08:31 +0300

    Push button interface.

---
+++ lib/PushButton/PushButton.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <PushButton.h>
+
+class PushButton
+{
+private:
+    /* data */
+public:
+    PushButton(pushButtonConfig cfg) {
+        
+    }
+};
---
+++ lib/PushButton/PushButton.h
@@ -0,0 +1,24 @@
+#ifndef PushButton_h
+#define PushButton_h
+
+struct pushButtonConfig
+{
+    // Min number of ms a signal should be seen
+    // to consider button pressed or switched
+    int minSignalDurationMs;
+};
+
+class PushButton
+{
+private:
+    /* data */
+public:
+    PushButton(pushButtonConfig cfg);
+
+    // Return state of a button (either LOW or HIGH)
+    int getState();
+
+    void processCurrentLevel(int level);
+};
+
+#endif
