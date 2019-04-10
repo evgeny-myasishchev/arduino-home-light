a8aae55409f8f8fcc5f417e52659e1e0fbae9903
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-04-10 08:28:01 +0300

    Some stubs

--- lib/PushButton/PushButton.h
+++ lib/PushButton/PushButton.h
@@ -1,13 +1,26 @@
 #ifndef PushButton_h
 #define PushButton_h
 
+#include <timers.h>
+
 struct pushButtonConfig
 {
     // Min number of ms a signal should be seen
     // to consider button pressed or switched
     int minSignalDurationMs;
 
-    
+    // Min number of times we've seen the signal
+    // to take it in action
+    int minSignalIterations;
+
+    nowMillisProc nowMillis;
+
+    pushButtonConfig(): 
+        minSignalDurationMs(200), 
+        minSignalIterations(10), 
+        nowMillis(defaultNowMillisProc())
+    {
+    }
 };
 
 class PushButton
@@ -20,7 +33,14 @@ public:
     // Return state of a button (either LOW or HIGH)
     int getState();
 
+    // Indicates wether state change recognised
+    bool hasStateChanged();
+
+    // 
     void processCurrentLevel(int level);
+
+    // Make current state active
+    void applyCurrentState();
 };
 
 #endif
