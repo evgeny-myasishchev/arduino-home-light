b6b0d9079be64194b7dc33d0fa6a51303634d96d
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-04-13 00:42:54 +0300

    Logs and random setup

--- src/tests/run_tests.cpp
+++ src/tests/run_tests.cpp
@@ -1,9 +1,14 @@
 #include <ArduinoUnit.h>
+#include <ArduinoLog.h>
 
 void setup()
 {
+  // A0 should not be connected
+  randomSeed(analogRead(0));
+
   Serial.begin(9600);
   while(!Serial) {} // Portability for Leonardo/Micro
+  Log.begin (LOG_LEVEL_VERBOSE, &Serial);
 
   Test::exclude("*");
   Test::include("dev_*");
