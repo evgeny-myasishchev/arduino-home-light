37306a0504729f4c2fbb66f3f28b3984b0b7f23c
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-04-28 15:51:24 +0300

    Formatting and constructor for state structure.

--- lib-universal/switch-service/switch-service.h
+++ lib-universal/switch-service/switch-service.h
@@ -4,7 +4,8 @@
 #include <Timers.h>
 #include <logger.h>
 
-namespace switch_service {
+namespace switch_service
+{
 
 #ifdef SWITCH_SERVICE_VERBOSE
 #define service_log logger_log
@@ -22,35 +23,50 @@ struct SwitchServiceConfig
     // to take it in action
     int minSignalIterations = 10;
 
-    Timers * timers = new Timers();
+    Timers *timers = new Timers();
 
     SwitchServiceConfig() {}
 
     SwitchServiceConfig(
-        int minSignalDurationMs, 
+        int minSignalDurationMs,
         int minSignalIterations,
-        Timers * timers
-    ) {
-            this -> minSignalDurationMs = minSignalDurationMs;
-            this -> minSignalIterations = minSignalIterations;
-            this -> timers = timers;
-        }
+        Timers *timers)
+    {
+        this->minSignalDurationMs = minSignalDurationMs;
+        this->minSignalIterations = minSignalIterations;
+        this->timers = timers;
+    }
 };
 
 struct SwitchStatus
 {
-    public:
-        int currentState = 0;
-        bool stateChanged = false;
-        int seenSignalTimes = 0;
-        unsigned int seenSignalSince = 0;
-};
+public:
+    int currentState = 0;
+    bool stateChanged = false;
+    int seenSignalTimes = 0;
+    unsigned int seenSignalSince = 0;
 
+    SwitchStatus() {}
+
+    SwitchStatus(
+        int currentState = 0,
+        bool stateChanged,
+        int seenSignalTimes,
+        unsigned int seenSignalSince)
+    {
+        this->currentState = currentState;
+        this->stateChanged = stateChanged;
+        this->currentState = currentState;
+        this->seenSignalTimes = seenSignalTimes;
+        this->seenSignalSince = seenSignalSince;
+    }
+};
 
 class SwitchService
 {
 protected:
     SwitchServiceConfig cfg;
+
 public:
     SwitchService(SwitchServiceConfig cfg);
     virtual void processSignal(int signal, SwitchStatus *switchStatus) = 0;
@@ -65,6 +81,6 @@ public:
     void applyStateChange(SwitchStatus *switchStatus);
 };
 
-}
+} // namespace switch_service
 
 #endif
--- test/switch-service-test.cpp
+++ test/switch-service-test.cpp
@@ -8,9 +8,52 @@
 #include "logger.h"
 #include "test-lib/random.h"
 
-TEST(switchService, ChangeStateWhenSeenSignalEnough)
+TEST(switchService, ChangeStateHighWhenSeenSignalEnough)
 {
     logger_log("TEST START: switchService, ChangeStateWhenSeenSignalEnough");
+
+    unsigned int nowMillis = test::randomNumber(100, 600);
+    int minSignalDurationMs = test::randomNumber(100, 600);
+    int minIterations = test::randomNumber(5, 10);
+    logger_log("minIterations: %d", minIterations);
+
+    FakeTimers * fakeTimers = new FakeTimers(nowMillis);
+
+    switch_service::SwitchServiceConfig cfg = switch_service::SwitchServiceConfig(
+        minSignalDurationMs,
+        minIterations,
+        fakeTimers);
+    switch_service::SwitchService *svc = new switch_service::PushButtonSwitchService(cfg);
+
+    switch_service::SwitchStatus status;
+
+    int durationIncrease = minSignalDurationMs / minIterations;
+
+    for (int i = 0; i < minIterations; i++)
+    {
+        svc->processSignal(HIGH, &status);
+        EXPECT_FALSE(status.stateChanged) << "Should not have changed state";
+        EXPECT_EQ(status.currentState, LOW) << "Should not have toggled state";
+        EXPECT_EQ(status.seenSignalSince, nowMillis) << "Should remember first time seen signal";
+        EXPECT_EQ(status.seenSignalTimes, i + 1) << "Should increment seen times";
+        fakeTimers->advance(durationIncrease);
+    }
+
+    logger_log("TEST: Completed minIterations stage");
+
+    fakeTimers->advance(durationIncrease);
+    svc->processSignal(HIGH, &status);
+    EXPECT_TRUE(status.stateChanged) << "Should have changed state";
+    EXPECT_EQ(status.currentState, HIGH) << "Should have HIGH value";
+    EXPECT_EQ(status.seenSignalTimes, minIterations + 1) << "Should increment seen times";
+    EXPECT_EQ(status.seenSignalSince, nowMillis) << "Should remember first time seen signal";
+
+    logger_log("TEST END: switchService, ChangeStateWhenSeenSignalEnough");
+}
+
+TEST(switchService, DoesntChangeStateOnLow)
+{
+    logger_log("TEST START: switchService, DoesntChangeStateOnLow");
 
     unsigned int nowMillis = test::randomNumber(100, 600);
     int minSignalDurationMs = test::randomNumber(100, 600);
