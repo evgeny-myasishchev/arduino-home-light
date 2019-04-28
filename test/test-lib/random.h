edaf8446fb5b245e01e9d1925d5895c7c7e73b5f
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-04-28 14:38:48 +0300

    Fixing random.

--- test/test-lib/random.h
+++ test/test-lib/random.h
@@ -9,7 +9,7 @@ T randomNumber(T min, T max)
 {
     std::random_device rd;
     std::mt19937 rng(rd());
-    std::uniform_int_distribution<T> uni(100, 600);
+    std::uniform_int_distribution<T> uni(min, max);
     return uni(rng);
 }
 
