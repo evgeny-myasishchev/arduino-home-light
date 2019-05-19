41a03082237e5bff20d0c816951e071f482ce3fe
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-05-19 23:20:22 +0300

    Defining mocks.

--- test/switches-router-test.cpp
+++ test/switches-router-test.cpp
@@ -1,4 +1,5 @@
 #include <switches.h>
+#include <switch-service.h>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <arduino-compat.h>
@@ -8,10 +9,25 @@
 namespace
 {
 
-class MockSignalReader : public switches::SignalReader
+using namespace switches;
+
+class MockSignalReader : public SignalReader
 {
 public:
     MOCK_METHOD1(read, void(int channel));
+};
+
+class MockSignalWriter : public SignalWriter
+{
+public:
+    MOCK_METHOD2(write, void(int address, int state));
+};
+
+class MockSwitchService : public SwitchService
+{
+public:
+    MOCK_METHOD2(processSignal, void(int signal, SwitchStatus *switchStatus));
+    MOCK_METHOD1(applyStateChange, void(SwitchStatus *switchStatus));
 };
 
 class SwitchesRouterTest : public ::testing::Test
@@ -24,6 +40,14 @@ protected:
 
 TEST_F(SwitchesRouterTest, processRoutes)
 {
+    MockSignalReader signalReader;
+    for (size_t i = 0; i < 2; i++)
+    {
+        EXPECT_CALL(signalReader, read(i));
+    }
+    
+    signalReader.read(0);
+    signalReader.read(1);
 }
 
 } // namespace
