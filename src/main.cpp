4f8dac6406dd9b61b66cfbe1309c27847ae4eae9
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-06-21 00:38:34 +0300

    Integrating router.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,49 @@
 #include <Arduino.h>
 #include "switches.h"
+#include "switches-router.h"
+#include "switch-service.h"
 
-switches::PCF8574IO io(0x20, 1, 0x21, 1);
+using namespace switches;
 
-void setup() {
-    io.init();
+PCF8574IO io(0x20, 1, 0x21, 1);
+SwitchesRouter *router;
+
+SwitchRoute createRoute(int targetAddress)
+{
+    int route1Targets[] = {0};
+    SwitchRoute route1(new SwitchStatus(), pstd::vector<int>(route1Targets));
+    return route1;
 }
 
-void loop() {
-    for (size_t i = 0; i < 8; i++)
-    {
-        auto state = io.read(i);
-        io.write(i, state);
-    }
+void setup()
+{
+    Serial.begin(9600);
+    while(!Serial) {}
+    logger_setup(&Serial);
+
+    SwitchRoute routesArray[] = {
+        createRoute(0),
+        createRoute(1),
+        createRoute(2),
+        createRoute(3),
+        createRoute(4),
+        createRoute(5),
+        createRoute(6),
+        createRoute(7)};
+    pstd::vector<SwitchRoute> routes(routesArray);
+
+    router = new SwitchesRouter(
+        pstd::vector<SwitchRoute>(routes),
+        SwitchRouterServices(
+            &io,
+            &io,
+            new PushButtonSwitchService(SwitchServiceConfig())));
+    io.init();
+
+    logger_log("Controller init. Total routes: %d", routes.size());
+}
+
+void loop()
+{
+    // router->processRoutes();
 }
