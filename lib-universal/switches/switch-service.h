c56df0fe86e518575c581eb7493b6e4b555e4dcb
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-07-14 14:24:00 +0300

    Some readme.

--- lib-universal/switches/switch-service.h
+++ lib-universal/switches/switch-service.h
@@ -48,6 +48,14 @@ public:
     virtual void applyStateChange(SwitchStatus *switchStatus) = 0;
 };
 
+/*
+Push button alg:
+
+1) If see signal for more than Xms and more than Ytimes, toggle signal state
+2) Keep the state unchanged if continuing seeing the signal
+3) Keep the state unchanged if stopped seeing the signal
+4) Go to 1 if started seeing the signal
+ */
 class PushButtonSwitchService : public SwitchService
 {
 public:
